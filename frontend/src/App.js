165885a9e8b8723d0db2aab3234e9b2606b54522
Nick D <nmdanner123@gmail.com>
2019-09-26 05:44:29 -0400

    oops

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,10 @@ class App extends Component{
       postList: [],
       roastList: [],
       boastList: [],
+      sortedList: [],
       roasts: false,
-      boasts: false
+      boasts: false,
+      sorted: false
     };
   }
 
@@ -30,51 +32,184 @@ class App extends Component{
           postList: res.data,
           roastList: res.data.filter(post => {
             return post.type_of_post == 'Roast'
-          })
-        
-        
+          }),
+          boastList: res.data.filter(post => {
+            return post.type_of_post == 'Boast'
+          }),
+          sortedList: res.data.sort((a,b) => (a.upvotes < b.upvotes) ? 1 : -1)
         })
       })
       .catch(err => console.log(err))
   }
 
   render(){
-    const { postList } = this.state
+    const { postList, roastList, boastList, sortedList } = this.state
 
-    const tempList = this.state.postList
-    console.log(postList)
-    console.log(tempList)
-
-    const roastsOnly = postList.filter(post => {
-      return post.type_of_post == 'Roast'
-    })
-
-    const boastsOnly = postList.filter(post => {
-      return post.type_of_post == 'Boast'
-    })
-
-    return (
-      <div>
-        <div style={{display: 'flex', justifyContent: 'space-around'}}>
-          <Button>Show only Roasts</Button>
-          <Button>Show only Boasts</Button>
-          <Button>Show most highly voted posts</Button>
+    if(!this.state.roasts && !this.state.boasts && !this.state.sorted){
+      return (
+        <div>
+          <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show only Roasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show only Boasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show most highly voted posts</Button>
+          </div>
+          <br/>
+          <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            { postList.map(post => (
+              <Post
+              type={post.type_of_post}
+              body={post.body}
+              upvotes={post.upvotes}
+              downvotes={post.downvotes}
+              created={post.created_at}
+              id={post.id}
+              refresh={this.refreshList}/>
+            ))}
+          </div>
         </div>
-        <br/>
-        <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          { postList.map(post => (
-            <Post
-            type={post.type_of_post}
-            body={post.body}
-            upvotes={post.upvotes}
-            downvotes={post.downvotes}
-            created={post.created_at}
-            id={post.id}
-            refresh={this.refreshList}/>
-          ))}
+      )
+    }else if(this.state.roasts){
+      return (
+        <div>
+          <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <Button onClick={() => {
+              this.setState({
+                roasts: true,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show only Roasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: true,
+                sorted: false
+              })
+            }}>Show only Boasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: true
+              })
+            }}>Show most highly voted posts</Button>
+          </div>
+          <br/>
+          <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            { roastList.map(post => (
+              <Post
+              type={post.type_of_post}
+              body={post.body}
+              upvotes={post.upvotes}
+              downvotes={post.downvotes}
+              created={post.created_at}
+              id={post.id}
+              refresh={this.refreshList}/>
+            ))}
+          </div>
         </div>
-      </div>
-    )
+      )
+    }else if(this.state.boasts){
+      return (
+        <div>
+          <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <Button onClick={() => {
+              this.setState({
+                roasts: true,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show only Roasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: true,
+                sorted: false
+              })
+            }}>Show only Boasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: true
+              })
+            }}>Show most highly voted posts</Button>
+          </div>
+          <br/>
+          <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            { boastList.map(post => (
+              <Post
+              type={post.type_of_post}
+              body={post.body}
+              upvotes={post.upvotes}
+              downvotes={post.downvotes}
+              created={post.created_at}
+              id={post.id}
+              refresh={this.refreshList}/>
+            ))}
+          </div>
+        </div>
+      )
+    }else if(this.state.sorted){
+      return (
+        <div>
+          <div style={{display: 'flex', justifyContent: 'space-around'}}>
+            <Button onClick={() => {
+              this.setState({
+                roasts: true,
+                boasts: false,
+                sorted: false
+              })
+            }}>Show only Roasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: true,
+                sorted: false
+              })
+            }}>Show only Boasts</Button>
+            <Button onClick={() => {
+              this.setState({
+                roasts: false,
+                boasts: false,
+                sorted: true
+              })
+            }}>Show most highly voted posts</Button>
+          </div>
+          <br/>
+          <div style = {{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            { sortedList.map(post => (
+              <Post
+              type={post.type_of_post}
+              body={post.body}
+              upvotes={post.upvotes}
+              downvotes={post.downvotes}
+              created={post.created_at}
+              id={post.id}
+              refresh={this.refreshList}/>
+            ))}
+          </div>
+        </div>
+      )
+    }  
   }
   
     
